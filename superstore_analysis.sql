0d08c445b03778a88a958141280e103458d53d93
masterbrighton <48730307+masterbrighton@users.noreply.github.com>
2026-05-15 11:17:34 +0200

    superstore queries update

--- superstore_analysis.sql
+++ superstore_analysis.sql
@@ -9,3 +9,25 @@ select extract(year from order_date) as order_year, sum(sales) as year_sales fro
 
 --What are top 10 customers?
 select customer_name, sum(sales) as purchase_amount from superstore group by customer_name order by purchase_amount desc limit 10;
+
+--What are the sales per region ?
+select region, sum(sales) as regional_sales from superstore group by region;
+
+--What is the profit per category ?
+select category, sum(sales) as category_profit from superstore group by category;
+
+--What are the sales per region
+select region, sum(sales) as regional_sales from superstore group by region;
+
+--What is the profit per category
+select category, sum(sales) as category_profit from superstore group by category;
+
+--Who is the top customer per region
+select customer_name, region, sales from superstore where sales in (select max(sales) from superstore group by region);
+
+--What are the running total of sales
+select order_date, sales, sum(sales) over (order by order_date, row_id) as running_profit from superstore;
+
+--Rank customers by revenue
+select customer_name, sales, rank() over (order by sales desc) from (
+    select customer_name, sum(sales) as sales from superstore group by customer_name );
